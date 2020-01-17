4d890580206b9eb32c901f8b9d07d4bd35371f45
xavierhazzardadmin <xavier@hazzard.org.uk>
2020-01-17 16:49:28 +0000

    Updates

---
+++ precache-manifest.a380dc820d77b0da829160b91d714bb7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83b1e7c207f416040d49f657e74261a8",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "a7cf06e7d450363cd833",
+    "url": "/react-gh-pages/static/js/2.5f97c95d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-gh-pages/static/js/2.5f97c95d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8d6e9baf281e6a330eb7",
+    "url": "/react-gh-pages/static/js/main.5c30511e.chunk.js"
+  },
+  {
+    "revision": "ecfd1d276f9241e07c49",
+    "url": "/react-gh-pages/static/js/runtime-main.d24436ce.js"
+  }
+]);
